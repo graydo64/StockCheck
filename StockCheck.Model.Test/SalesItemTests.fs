59cfda42a4184317a72ff62339433e703649e593
graydo64 <personal@graemewilson.eu>
2013-04-22 16:45:37 +0100

    Moved SalesItem tests to FsUnit.

--- StockCheck.Model.Test/SalesItemTests.fs
+++ StockCheck.Model.Test/SalesItemTests.fs
@@ -25,3 +25,30 @@ type ``Given that the item is draught and the cost of sale is greater than zero`
         ``The cost per unit of sale should be calculated correctly`` () =
             salesItem.CostPerUnitOfSale |> should equal (float costPerContainer / saleUnitsPerContainer)
 
+[<TestFixture>]
+type ``Given that the item is spirit and the cost of sale is greater than zero`` () =
+    let containerSize = 0.7
+    let unitOfSale = 0.035
+    let costPerContainer = 22m
+    let saleUnitsPerContainer = containerSize / unitOfSale
+    let salesItem = new SalesItem(ContainerSize = containerSize, UnitOfSale = unitOfSale, CostPerContainer = costPerContainer)
+
+    [<Test>] member x.
+        ``The cost per unit of sale should be calculated correctly`` () =
+            salesItem.CostPerUnitOfSale |> should equal (float costPerContainer / saleUnitsPerContainer)
+
+[<TestFixture>]
+type ``Given that the Sales Price is zero`` () =
+    let salesItem = new SalesItem(SalesPrice = 0m)
+
+    [<Test>] member x.
+        ``The GP is zero`` () =
+            salesItem.IdealGP |> should equal 0
+
+[<TestFixture>]
+type ``Given that the Sales Price double the Cost price`` () =
+    let salesItem = new SalesItem(SalesPrice = 2m, ContainerSize = 1., UnitOfSale = 1., CostPerContainer = 1m)
+
+    [<Test>] member x.
+        ``The GP is 50 percent`` () =
+            salesItem.IdealGP |> should equal 0.5
